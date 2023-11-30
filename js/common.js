2142cae074dea40042a0662dfbe7873d2d2c9da2
five <five.950608@gmail.com>
2023-11-30 15:35:52 +0900

    v1.0

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(function(){
+    console.log("load")
+    $(".main-nav").mouseenter(function(){
+        $(".drop").stop().slideDown();
+    })
+    $(".main-nav").mouseleave(function(){
+        $(".drop").stop().slideUp();
+    })
+})
